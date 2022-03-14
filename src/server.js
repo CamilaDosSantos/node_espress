ebad56c296dec865ca0f8a46c272ccd0d3445307
camila <tec.camiladossantos@gmail.com>
2022-03-14 15:56:17 -0300

    put

--- src/server.js
+++ src/server.js
@@ -10,5 +10,7 @@ console.log('example app listening on port ${port}')
 app.post('/', (req, res) => {
     res.send('Got a POST request')
   })
-  
+  app.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user')
+  })
 
